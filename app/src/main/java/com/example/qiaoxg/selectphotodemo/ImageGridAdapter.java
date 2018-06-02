76af8cd0acbcb61fc7c3eb21f433bd0d148f52e7
Qiaoxg <qxg.bxf@163.com>
2018-06-02 14:57:12 +0800

    结果页图片展示适配屏幕

--- app/src/main/java/com/example/qiaoxg/selectphotodemo/ImageGridAdapter.java
+++ app/src/main/java/com/example/qiaoxg/selectphotodemo/ImageGridAdapter.java
@@ -1,10 +1,14 @@
 package com.example.qiaoxg.selectphotodemo;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.qiaoxg.selectphotodemo.model.ImageInfo;
 import com.squareup.picasso.Picasso;
@@ -16,11 +20,28 @@ import java.util.ArrayList;
 public class ImageGridAdapter extends CommonListAdapter<ImageInfo> {
 
     private int maxSize = 9;
+    private int itemWidth = 90;
+    private RelativeLayout.LayoutParams params;
 
     public ImageGridAdapter(Context context, int maxSize) {
         super(context);
         this.mContext = context;
         this.maxSize = maxSize;
+
+        itemWidth = (getScreenWidth(context) - dp2px(context, 20)) / 3;
+        params = new RelativeLayout.LayoutParams(itemWidth, itemWidth);
+    }
+
+    private int getScreenWidth(Context context) {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.widthPixels;
+    }
+
+    private int dp2px(Context context, float dpVal) {
+        return (int) TypedValue.applyDimension(1, dpVal, context.getResources().getDisplayMetrics());
     }
 
     //获取源图片
@@ -73,6 +94,8 @@ public class ImageGridAdapter extends CommonListAdapter<ImageInfo> {
             holder = (ViewHolder) convertView.getTag();
         }
 
+        holder.image.setLayoutParams(params);
+
         ImageInfo info = mList.get(position);
         if (info != null) {
 
@@ -82,7 +105,7 @@ public class ImageGridAdapter extends CommonListAdapter<ImageInfo> {
                         .load(imageFile)
                         .placeholder(R.drawable.camerasdk_pic_loading)
                         .error(R.drawable.camerasdk_pic_loading)
-                        .resize(100, 100)
+                        .resize(itemWidth, itemWidth)
                         .centerCrop()
                         .into(holder.image);
             } else {
